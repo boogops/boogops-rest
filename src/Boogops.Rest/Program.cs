279694129b94c88b0e1b56cf6793af2ce897e48b
Heath Turnage <ibanezht@icloud.com>
2021-12-11 20:48:50 -0500

    Moving to precious. :alien:

--- src/Boogops.Rest/Program.cs
+++ src/Boogops.Rest/Program.cs
@@ -12,13 +12,11 @@ builder.Services.AddAutoMapper(typeof(BoogopsProfile));
 
 builder.Services.AddControllers();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
